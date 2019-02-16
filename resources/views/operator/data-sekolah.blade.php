4844ec6a8fd7b995c09c9dc7bb872167bf7c7690
juliotalibonso <talibonsojulio@gmail.com>
2019-02-16 18:08:54 +0800

    Halaman data sekolah fix vers 1

--- resources/views/operator/data-sekolah.blade.php
+++ resources/views/operator/data-sekolah.blade.php
@@ -134,6 +134,70 @@
 
 </form>
 
+<br><br>
 
+<table class="table">
+    <thead>
+        <tr>
+            <th><abbr title="Position">No.</abbr></th>
+            <th><abbr title="Position">Wilayah</abbr></th>
+            <th><abbr title="Position">Jumlah</abbr></th>
+            <th><abbr title="Position">TK/PAUD</abbr></th>
+            <th><abbr title="Position">SD</abbr></th>
+            <th><abbr title="Position">SMP</abbr></th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Kec. Tomohon Utara</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Kec. Tomohon Selatan</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Kec. Tomohon Tengah</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Kec. Tomohon Barat</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Kec. Tomohon Selatan</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <th>Total</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
 
 @endsection
