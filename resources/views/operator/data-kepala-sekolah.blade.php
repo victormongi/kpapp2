9a7406b66b785eaf353804b41f863c617661f23c
Victor Mongi <victor.mongi@gmail.com>
2019-02-18 06:38:44 +0800

    add data operator dan kepsek

--- resources/views/operator/data-kepala-sekolah.blade.php
+++ resources/views/operator/data-kepala-sekolah.blade.php
@@ -10,7 +10,8 @@
 <h1 class="title is-1">Data Kepala Sekolah</h1>
 
 <button class="button is-primary is-large" @click="toggleShow">Tambah Data Kepala Sekolah</button>
-    <div class="column is-5" v-show="formShow">    <form action="/operator/data-kepala-sekolah" method="POST">
+    <div class="column is-5" v-show="formShow">
+        <form action="/operator/data-kepala-sekolah" method="post" enctype="multipart/form-data">
         @csrf
         <div class="field">
             <label class="label" for="nama_kepala_sekolah">Nama</label>
